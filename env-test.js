90d683ec4db9aa6edca60bbc178f73564d306e15
FrancoGomez <mgomez21699@gmail.com>
2023-07-20 11:34:51 -0300

    Add environment files

---
+++ env-test.js
@@ -0,0 +1,13 @@
+const BACKEND_PORT = 8080;
+
+const BACKEND_URL = `http://localhost:${BACKEND_PORT}`;
+
+const FRONT_PORT = 5500;
+
+const FRONTEND_URL = `http://localhost:${FRONT_PORT}`;
+
+const GOOGLE_KEY = 'AIzaSyAk3Fkm8bwFXfxLCS0Pl8XiZTBE6WoAZSA';
+
+module.exports = {
+  BACKEND_PORT, BACKEND_URL, FRONT_PORT, FRONTEND_URL, GOOGLE_KEY,
+};
---
+++ env.js
@@ -0,0 +1,9 @@
+export const BACKEND_PORT = 8080;
+
+export const BACKEND_URL = `http://localhost:${BACKEND_PORT}/clubs`;
+
+export const FRONT_PORT = 5500;
+
+export const FRONTEND_URL = `http://localhost:${FRONT_PORT}`;
+
+export const GOOGLE_KEY = 'AIzaSyAk3Fkm8bwFXfxLCS0Pl8XiZTBE6WoAZSA';
