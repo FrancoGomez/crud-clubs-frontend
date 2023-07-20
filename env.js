6e631dbda4d289589b1833a75fe30eb333ff3e55
FrancoGomez <mgomez21699@gmail.com>
2023-07-20 14:11:45 -0300

    Delete variables because they were no longer used

--- env.js
+++ env.js
@@ -1,9 +1,5 @@
 export const BACKEND_PORT = 8080;
 
-export const BACKEND_URL = `http://localhost:${BACKEND_PORT}/clubs`;
-
-export const FRONT_PORT = 5500;
-
-export const FRONTEND_URL = `http://localhost:${FRONT_PORT}`;
+export const BACKEND_URL = 'https://crud-clubs-backend-production.up.railway.app';
 
 export const GOOGLE_KEY = 'AIzaSyAk3Fkm8bwFXfxLCS0Pl8XiZTBE6WoAZSA';
